3da166070f961f0c6de80ced3b9a2fbef988b302
HOANGNBD <Duchoang020120@gmail.com>
2023-05-16 19:32:54 +0700

    first commit

---
+++ models/index.js
@@ -0,0 +1,21 @@
+const Category = require("./Category");
+const Supplier = require("./Supplier");
+const Customer = require("./Customer");
+const Employee = require("./Employee");
+const Product = require("./Product");
+const Order = require("./Order");
+const Media = require("./Media");
+const Conversation = require("./Conversation");
+const Message = require("./Message");
+
+module.exports = {
+  Category,
+  Product,
+  Supplier,
+  Customer,
+  Employee,
+  Order,
+  Media,
+  Conversation,
+  Message,
+};
